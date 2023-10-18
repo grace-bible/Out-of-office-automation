8fcada6ed16514a2fb6399a52a188e4a655ca59b
Josh McKenna <joshmckenna@grace-bible.org>
2023-10-18 18:09:28 -0500

    Initial commit with GBC base config defined

--- Time-off-request-app.js
+++ Time-off-request-app.js
@@ -1,24 +1,78 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
 const Header = {
-  Tidsstempel: 'Tidsstempel',
-  Mailadresse: 'Mailadresse',
-  Name: 'Name',
+  Timestamp: 'Timestamp',
+  FullName: 'Name',
+  EmailAddress: 'Email Address',
+  Campus: 'Campus',
   StartDate: 'Start date',
-  EndDate: 'End date',
-  Approval: 'Approval',
+  EndDate: 'Return date',
+  Reason: 'Reason',
+  Description: 'Brief description',
+  SuperAddress: 'Supervisor email',
+  SupervisorApproval: 'My supervisor has already approved this request',
+  HRApproval: 'HR approval',
+  EventCreated: 'Calendar event status',
 };
 
-const Approval = {
+const Reason = {
+  Personal: 'Personal',
+  Professional: 'Professional',
+  DWTL: 'DWTL',
+};
+
+const Campus = {
+  AND: 'Anderson',
+  SW: 'Southwood',
+  CRK: 'Creekside',
+  MT: 'Midtown',
+  SYS: 'System',
+};
+
+const SupervisorApproval = {
   Approved: 'Approved',
   NotApproved: 'Not approved',
 };
 
+const HRApproval = {
+  Approved: 'Approved',
+  NotApproved: 'Not approved',
+};
+
+const EventCreated = {
+  NotCreated: 'Event not created',
+  Created: 'Event created',
+};
+
+/**
+* Add custom menu items when opening the sheet.
+*/
 function onOpen() {
   SpreadsheetApp.getUi()
-      .createMenu('🏝 Vacation')
-      .addItem('Notify employees', 'data')
+      .createMenu('Approval functions')
+      .addItem('Form setup', 'formSetup')
+      .addItem('Column setup', 'columnSetup')
+      .addItem('Create calendar events', 'create')
       .addToUi();
 }
 
+/**
+   * Set up the "Request time off" form, and link the form's trigger to 
+   * optionally send an email to an additional address (like a manager).
+   */
 function formSetup() {
   let sheet = SpreadsheetApp.getActiveSpreadsheet();
   if (sheet.getFormUrl()) {
@@ -34,36 +88,103 @@ function formSetup() {
   }
 
   // Create the form.
-  let form = FormApp.create('Request time off')
+  let form = FormApp.create('Out of office (OOO) request')
       .setCollectEmail(true)
       .setDestination(FormApp.DestinationType.SPREADSHEET, sheet.getId())
       .setLimitOneResponsePerUser(false);
 
-  form.addTextItem().setTitle(Header.Name).setRequired(true);
+  form.addTextItem().setTitle(Header.FullName).setRequired(true);
+  form.addListItem().setTitle(Header.Campus).setChoiceValues(Object.values(Campus)).setRequired(false);
   form.addDateItem().setTitle(Header.StartDate).setRequired(true);
   form.addDateItem().setTitle(Header.EndDate).setRequired(true);
-  const destinationId = form.getDestinationId();
+  form.addListItem().setTitle(Header.Reason).setChoiceValues(Object.values(Reason)).setRequired(true);
+  form.addTextItem().setTitle(Header.Description).setRequired(false);
+  form.addTextItem().setTitle(Header.SuperAddress).setRequired(true);
+  
+  let item = form.addCheckboxItem();
 
-  // Open the Google Workbook and iterate through each sheet
-  const formSpreadsheet = SpreadsheetApp.openById(destinationId);
-  const [formSheet] = formSpreadsheet.getSheets().filter((sheet) => {
-      // Returns the URL of the associated Google form
-      // that is sending its user responses to this sheet
-      let range = sheet.getRange(1, sheet.getLastColumn() + 1);
-
-      // Create the header header name.
-      range.setValue(Header.Approval);
-
-      // If we pass choices to the function, create validation rules.
-      let rule = SpreadsheetApp.newDataValidation()
-        .requireValueInList(Object.values(Approval))
-        .build();
-
-      range.offset(sheet.getFrozenRows(), 0, sheet.getMaxRows())
-        .setDataValidation(rule);
-    });
+  item.setTitle(Header.SupervisorApproval)
+  .setChoices([
+    item.createChoice('Approved'),
+  ])
+  .showOtherOption(false)
+  .setRequired(true);
 }
 
+
+/**
+ * Creates an "HR Approved" and "Calendar event status" column
+ */
+function columnSetup() {
+  let sheet = SpreadsheetApp.getActiveSheet();
+
+  appendColumn(sheet, Header.HRApproval, Object.values(HRApproval));
+  appendColumn(sheet, Header.EventCreated, Object.values(EventCreated));
+}
+
+
+/**
+ * Appends a new column.
+ * 
+ *  @param {SpreadsheetApp.Sheet} sheet - tab in sheet.
+ *  @param {string} headerName - name of column.
+ *  @param {(string[] | null)} maybeChoices - optional drop down values for validation.
+ */
+function appendColumn(sheet, headerName, maybeChoices) {
+  let range = sheet.getRange(1, sheet.getLastColumn() + 1);
+
+  // Create the header header name.
+  range.setValue(headerName);
+
+  // If we pass choices to the function, create validation rules.
+  if (maybeChoices) {
+    let rule = SpreadsheetApp.newDataValidation()
+        .requireValueInList(maybeChoices)
+        .build();
+
+    range.offset(sheet.getFrozenRows(), 0, sheet.getMaxRows())
+        .setDataValidation(rule);
+  }
+}
+
+/**
+* Checks the creation status of each entry and, if not created,
+* creates a new calendar item accordingly.
+*/
+function create() {
+  let sheet = SpreadsheetApp.getActiveSheet();
+  let dataRange = sheet.getDataRange().getValues();
+  let headers = dataRange.shift();
+
+  validateSheetHeaders(headers, Header);
+
+  let rows = dataRange
+      .map((row, i) => asObject(headers, row, i))
+      .filter(row => row[Header.EventCreated] != EventCreated.Created)
+      .map(process)
+      .map(row => writeRowToSheet(sheet, headers, row));
+}
+
+/**
+* Validate that the sheet headers match a schema.
+*/
+function validateSheetHeaders(headers, schema) {
+  for (let header of Object.values(schema)) {
+    if (!headers.includes(header)) {
+      throw `🦕 ⚠️ Header "${header}" not found in sheet: ${JSON.stringify(headers)}`;
+    }
+  }
+}
+
+/**
+* Convert the row arrays into objects.
+* Start with an empty object, then create a new field
+* for each header name using the corresponding row value.
+* 
+* @param {string[]} headers - list of column names.
+* @param {any[]} rowArray - values of a row as an array.
+* @param {int} rowIndex - index of the row.
+*/
 function asObject(headers, rowArray, rowIndex) {
   return headers.reduce(
     (row, header, i) => {
@@ -72,51 +193,102 @@ function asObject(headers, rowArray, rowIndex) {
     }, {rowNumber: rowIndex + 1});
 }
 
-function data(){
-  let sheet = SpreadsheetApp.getActiveSheet();
-  let dataRange = sheet.getDataRange().getValues();
-  let headers = dataRange.shift();
-
-  dataRange
-      .map((row, i) => asObject(headers, row, i))
-      .map(process)
-      
-}
-
-
+/**
+* Checks if a row is marked as "approved". If approved a calendar
+* event is created for the user. If not approved, an email
+* notification is sent.
+* 
+* @param {Object} row - values in a row.
+* @returns {Object} the row with a "notified status" column populated.
+*/
 function process(row) {
-  let name = row[Header.Name];
-  let email = row[Header.Mailadresse];
+  let email = row[Header.EmailAddress];
+  let name = row[Header.FullName];
+  let supervisor = row[Header.SuperAddress];
+  let guestEmails = `grace-bible.org_323330343338383235@resource.calendar.google.com,`;
   let startDate = row[Header.StartDate];
+    // Create a new variable to store the incremented date.
+    let incrementStartDate = new Date(startDate);
+    // Increment the `incrementStartDate` variable by 1 day.
+    incrementStartDate.setDate(incrementStartDate.getDate() + 1);
   let endDate = row[Header.EndDate];
-  let approval = row[Header.Approval];
-  let message = `Your vacation time request from `
+    // Create a new variable to store the incremented date.
+    let incrementEndDate = new Date(endDate);
+    // Increment the `incrementStartDate` variable by 1 day.
+    incrementEndDate.setDate(incrementEndDate.getDate() + 1);
+  let reason = row[Header.Reason];
+  let description = row[Header.Description];
+  let superApproval = (row[Header.SupervisorApproval]);
+  let hrApproval = (row[Header.HRApproval]);
+  let eventName = `${name} - ${reason}`;
+  let eventDescription = `${superApproval} by ${supervisor}\n\n`
+      + `${description}`;
+  let message = `Your ${reason} request was ${hrApproval} for `
       + `${startDate.toDateString()} to `
-      + `${endDate.toDateString()}: ${approval}`;
+      + `${endDate.toDateString()}\n\n`
+      + `${description}`;
 
-  if (approval == Approval.NotApproved) {
+  // /* Check if the user has a calendar. */
+  // const calendar = CalendarApp.getCalendarById(email);
+  // if (!calendar) {
+  //     // The user does not have a calendar.
+  //     Logger.log(`User does not have a calendar: ${email}`);
+  //     // Display a message to the user.
+  //     UiApp.alert(`User does not have a calendar. Please contact IT support before using this script.`);
+  //     // Skip creating the calendar event.
+  //     return row;
+  // }
+
+  /* Confirm that the supervisor approved. */
+  if (superApproval == SupervisorApproval.NotApproved) {
     // If not approved, send an email.
-
-    let subject = 'Your vacation time request was NOT approved';
+    let subject = 'Your vacation time request failed, contact Josh McKenna';
     MailApp.sendEmail(email, subject, message);
+    row[Header.EventCreated] = EventCreated.Created;
+
+    Logger.log(`Not approved, email sent, row=${JSON.stringify(row)}`);
   }
 
-  else if (approval == Approval.Approved) {
+  else if (superApproval == SupervisorApproval.Approved) {
     // If approved, create a calendar event.
+    CalendarApp.getCalendarById(email)
+        .createAllDayEvent(
+            eventName,
+            incrementStartDate,
+            incrementEndDate,
+            {
+              description: eventDescription,
+              guests: guestEmails,
+              sendInvites: true,
+            });
+  
+      // // Send a confirmation email.
+      // let subject = 'Confirmed, your vacation time request has been approved!';
+      // MailApp.sendEmail(email, subject, message, {cc: supervisor});
+  
+      row[Header.EventCreated] = EventCreated.Created;
+  
+      Logger.log(`Approved calendar event created, row=${JSON.stringify(row)}`);
+  }
 
-    CalendarApp.getCalendarById("")
-      .createAllDayEvent(
-          name + ' on vacation',
-          startDate,
-          endDate,
-          {
-            description: message,
-          });
+  else {
+    row[Header.EventCreated] = EventCreated.NotCreated;
 
-    // Send a confirmation email.
-    let subject = 'Confirmed, your vacation time request has been approved!';
-    MailApp.sendEmail(email, subject, message);
+    Logger.log(`No action taken, row=${JSON.stringify(row)}`);
   }
 
   return row;
+}
+
+/**
+* Rewrites a row into the sheet.
+* 
+* @param {SpreadsheetApp.Sheet} sheet - tab in sheet.
+* @param {string[]} headers - list of column names.
+* @param {Object} row - values in a row.
+*/
+function writeRowToSheet(sheet, headers, row) {
+let rowArray = headers.map(header => row[header]);
+let rowNumber = sheet.getFrozenRows() + row.rowNumber;
+sheet.getRange(rowNumber, 1, 1, rowArray.length).setValues([rowArray]);
 }
